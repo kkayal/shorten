b94f7db4bc8cc0c0812a24807032ac27b5b7a4dc
Kağan Kayal <kagan@kagan.de>
2018-02-17 17:35:24 +0100

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# shorten
-A crate containing conviniance functions and macros to simplify repetitive code.
+# Objective:
+
+A collection of convenience functions, macros and traits to shorten repetitive code.
+
+# Status
+
+Experimental
+
+# Version
+
+0.1.0
+
+## Example 1:
+
+Shorten the convertion to a String.
+
+`s!("Hello")` is the same as `String::from("Hello"))`
+
+## Example 2:
+
+Concatenate two string(s) (slices) and return a **string slice**.
+
+`ss!("Hello", ", world")` is the same as `"Hello, world"`
+
+The same macro works also with an arbitrarz combination of String objects and string slices
+
+```
+#[macro_use] extern crate shorten;
+let s1 = s!("Hello");
+let s2 = s!(", world");
+assert_eq!(ss!(s1, s2), "Hello, world");
+```
---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+//! # Objective:
+//! 
+//! A collection of convenience functions, macros and traits to shorten repetitive code.
+//! 
+//! # Status
+//! 
+//! Experimental
+//! 
+//! # Version
+//! 
+//! 0.1.0
+//! 
+//! ## Example 1:
+//! 
+//! Shorten the convertion to a String.
+//! 
+//! `s!("Hello")` is the same as `String::from("Hello"))`
+//! 
+//! ## Example 2:
+//! 
+//! Concatenate two string(s) (slices) and return a **string slice**.
+//! 
+//! `ss!("Hello", ", world")` is the same as `"Hello, world";`
+//! 
+//! The same macro works also with an arbitrarz combination of String objects and string slices
+//! 
+//! ```rust
+//! // #[macro_use] extern crate shorten;
+//! // let s1 = s!("Hello");
+//! // let s2 = s!(", world");
+//! // assert_eq!(ss!(s1, s2), "Hello, world");
+//! ```
+
+// Shorten String::from("hello") to s!("hello")
+macro_rules! s { ( $x:expr ) => { String::from($x); }; }
+
+// Concatenate two string(s) (slices) and return a string slice
+macro_rules! ss {	($x:expr, $y:expr) => ( &*format!("{}{}", $x, $y); ) }
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn s() {
+		assert_eq!(s!("Hello"), String::from("Hello"));
+	}
+
+	#[test]
+	fn ss() {
+		assert_eq!(ss!("Hello", ", world"), "Hello, world");
+
+		let s1 = s!("Hello");
+		let s2 = s!(", world");
+		assert_eq!(ss!(s1, s2), "Hello, world");
+	}
+}
