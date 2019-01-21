eb87724d8d60e7d5ebafa8746fe6183de08dc138
DLR\kaya_ka <kagan.kayal@dlr.de>
2019-01-21 13:26:39 +0100

    Fixed a documentation bug, which showed when tested with rustc 1.31.1. Added Rust edition 2018 to cargo.toml

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@
 //! 
 //! # Version
 //! 
-//! 0.1.3
+//! 0.1.4
 //! 
 //! ## Example 1:
 //! 
@@ -44,11 +44,13 @@
 //! let s = String::from_utf8(v).expect("Found invalid UTF-8");
 //!
 //! // Option 2: Lossy. It turns invalid UTF-8 bytes into � and so no error handling is required
-//! let s = String::from_utf8_lossy(&v[..]).into_owned(); // Does NOT consume v ???
+//! let v = fread(".gitignore").unwrap();
+//! let s = String::from_utf8_lossy(&v[..]).into_owned();
 //!
 //! // Option 3: Convert to a u8 array (not Vec) and then to a string slice. 
 //! // The conversion is in-place, and does not require an allocation. You can create a String from the slice if necessary.
-//! let s = std::str::from_utf8(&v[..]).unwrap(); // Does NOT consume v ???
+//! let v = fread(".gitignore").unwrap();
+//! let s = std::str::from_utf8(&v[..]).unwrap();
 //! ```
 
 // Shorten String::from("hello") to s!("hello")
